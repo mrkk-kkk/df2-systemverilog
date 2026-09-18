efd70d89925a8abd16dfe955703dff1944183e85
mrk_kk <128633934+mrkk-kkk@users.noreply.github.com>
2026-09-18 18:28:04 +0300

    Update task4.sv

--- week2/task4/task4.sv
+++ week2/task4/task4.sv
@@ -1,1 +1,20 @@
+module mult4x4 (input  logic [3:0] A,
+                input  logic [3:0] B,
+                output logic [7:0] product);
 
+ 
+  logic [3:0] lines [3:0];
+  logic [3:0] rows  [3:0];
+  
+  for(genvar i = 0; i < 4 ; i++) begin: n_line   
+    for(genvar j = 0; j < 4 ; j++) begin: n_row   
+      
+      assign lines[i][j] = A[j] & B[i];
+        
+    end
+  end
+  
+  
+  
+
+endmodule
