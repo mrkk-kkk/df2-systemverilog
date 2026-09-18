febf08325504b5a1d62a18d7f4463c9953893b1b
mrk_kk <128633934+mrkk-kkk@users.noreply.github.com>
2026-09-18 17:48:03 +0300

    Implement 3-bit counter module with reset and enable

--- week2/task3/task3.sv
+++ week2/task3/task3.sv
@@ -1,1 +1,15 @@
-
+module counter3bit (input  logic clk,
+                    input  logic reset,
+                    input  logic enable,
+                    output logic [2:0] count);
+   
+  always_ff @(posedge clk) 
+  begin: counter  
+   if (reset)
+      count <= 3'b000;
+   else
+   if (enable)
+      count <= count + 3'b001;
+  end
+  
+endmodule
