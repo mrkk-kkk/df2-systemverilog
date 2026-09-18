55cb812826094e26fb4ddfe6d5af6d7b6fea02bd
mrk_kk <128633934+mrkk-kkk@users.noreply.github.com>
2026-09-18 17:40:34 +0300

    Update task1.sv

--- week2/task1/task1.sv
+++ week2/task1/task1.sv
@@ -1,1 +1,26 @@
-
+module adder_sub #(parameter WIDTH = 8)
+                  (input  logic [WIDTH-1:0] a,
+                   input  logic [WIDTH-1:0] b,
+                   input  logic sub,
+                   output logic cout, 
+                   output logic [WIDTH-1:0] result);
+  
+  logic [WIDTH:0] C;
+ 
+  assign C[0] = sub;
+  
+  for(genvar i = 0; i < WIDTH; i++) begin: full_adders
+      
+    logic bxor;
+    assign bxor = b[i] ^ sub;
+      
+    full_adder fa(.sum(result[i]), 
+                  .a(a[i]), 
+                  .b(bxor), 
+                  .cin(C[i]), 
+                  .cout(C[i+1])); 
+  end  
+  
+  assign cout = C[WIDTH];
+  
+endmodule
