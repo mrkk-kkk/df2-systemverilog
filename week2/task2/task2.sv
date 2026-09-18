9a4bd0d4f4843e03f7a666aec86e7cbfd685b02d
mrk_kk <128633934+mrkk-kkk@users.noreply.github.com>
2026-09-18 17:44:42 +0300

    Add 4-bit register module with reset and enable

--- week2/task2/task2.sv
+++ week2/task2/task2.sv
@@ -1,1 +1,16 @@
-
+module register4bit (input  logic clk,
+                     input  logic reset,
+                     input  logic enable,
+                     input  logic [3:0] D,
+                     output logic [3:0] Q);
+   
+  always_ff @(posedge clk) 
+  begin: register  
+   if (reset)
+      Q <= 4'b0000;
+   else
+   if (enable)
+      Q <= D;
+  end
+  
+endmodule
