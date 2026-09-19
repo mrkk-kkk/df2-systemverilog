4235796b6472038ad26a28989f71c7ba6099c7bf
mrk_kk <128633934+mrkk-kkk@users.noreply.github.com>
2026-09-19 16:21:57 +0300

    Update task4.sv

--- week2/task4/task4.sv
+++ week2/task4/task4.sv
@@ -4,17 +4,32 @@ module mult4x4 (input  logic [3:0] A,
 
  
   logic [3:0] lines [3:0];
-  logic [3:0] rows  [3:0];
+  logic [3:0] rows  [2:0];
+  logic [4:0] C 	  [2:0];
   
-  for(genvar i = 0; i < 4 ; i++) begin: n_line   
-    for(genvar j = 0; j < 4 ; j++) begin: n_row   
+  for(genvar i = 0; i < 4 ; i++) begin   
+    for(genvar j = 0; j < 4 ; j++) begin
       
       assign lines[i][j] = A[j] & B[i];
         
     end
   end
   
+  assign product[0] = lines[0][0];
   
+  for(genvar i = 0; i < 3 ; i++) begin
+    
+    assign C[i][0] = 1'b0;
+    
+    for(genvar j = 0; j < 4 ; j++) begin
+    
+      full_adder fa(.sum(rows[i][j]), 		
+                    .a(lines[i][j+1]), 
+                    .b(lines[i+1][j]), 
+                    .cin(C[i][j]), 
+                    .cout(C[i][j+1]));
   
-
+    end
+  end
+  
 endmodule
